2f8fe1bccb708bf6063dbf872511792106f9dc0b
Kelli <keldavis5@yahoo.com>
2018-05-02 14:20:06 -0400

    fix crash error

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -13,12 +13,14 @@ import com.udacity.sandwichclub.utils.JsonUtils;
 
 import org.json.JSONException;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
-    private ImageView imageIv;
     private TextView originTv, alsoTv, descriptionTv, ingredientsTv;
+    Sandwich sandwich;
 
 
     @Override
@@ -26,13 +28,18 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ImageView imageIv = findViewById(R.id.image_iv);
+        originTv = findViewById(R.id.origin_tv);
+        alsoTv = findViewById(R.id.also_known_tv);
+        descriptionTv = findViewById(R.id.description_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
+        assert intent != null;
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -42,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = null;
+
         try {
             sandwich = JsonUtils.parseSandwichJson(json);
         } catch (JSONException e) {
@@ -57,7 +64,9 @@ public class DetailActivity extends AppCompatActivity {
         populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                // Use this icon before the data is populated. If no image, use icon
+                .placeholder(R.mipmap.ic_launcher)
+                .into(imageIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -68,6 +77,30 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     private void populateUI() {
+        descriptionTv.setText(sandwich.getDescription().equals("") ? "No Info" : sandwich.getDescription());
+        originTv.setText(sandwich.getPlaceOfOrigin().equals("") ? "No Info" : sandwich.getDescription());
+        appendListToTextView(alsoTv, sandwich.getAlsoKnownAs());
+        appendListToTextView(ingredientsTv, sandwich.getIngredients());
+    }
 
+    /*
+    * This method takes in a textview and a string list.
+    * It will append a list of ingredients and nicknames
+    * (also known as) to the textview.
+    * */
+    public void appendListToTextView(TextView textView, List<String> list) {
+        int listSize = list.size();
+        if(listSize == 0) {
+            // If there is no information to display
+            textView.append("No Info");
+        }
+
+        for(int i = 0; i < listSize; i++) {
+            if (i == listSize - 1) {
+                textView.append(list.get(i));
+            }else {
+                textView.append(list.get(i) + "\n");
+            }
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -14,12 +14,13 @@ import java.util.List;
 public class JsonUtils {
     private static final String TAG = JsonUtils.class.getName();
 
-    /*
-    * This method takes in a String json parameter
-    * Returns a sandwich object
-    * throws a JSONException
+    /* The sandwich object takes in a JSON string. Six parameters make up one
+    * sandwich.
+    * This method throws a JSONException
+    *
+    * Returns a Sandwich object
     * */
-    public static Sandwich parseSandwichJson(String jSon) throws JSONException {
+    public static Sandwich parseSandwichJson(String json) throws JSONException {
         final String KEY_NAME = "name";
         final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
         final String KEY_DESCRIPTION = "description";
@@ -28,21 +29,13 @@ public class JsonUtils {
         final String KEY_INGREDIENTS = "ingredients";
         final String KEY_ALSO = "alsoKnownAs";
 
-        JSONObject sandwich = new JSONObject(jSon);
-
-        // Get the name of the JSON object
+        JSONObject sandwich = new JSONObject(json);
         JSONObject name = sandwich.getJSONObject(KEY_NAME);
 
-        // Get the origin of the JSON object
         String origin = sandwich.getString(KEY_PLACE_OF_ORIGIN);
-
-        // Get the description of the JSON object
         String description = sandwich.getString(KEY_DESCRIPTION);
-
-        // Get the image of the JSON object
         String image = sandwich.getString(KEY_IMAGE);
 
-        // Get the main name
         String mainName = name.getString(KEY_MAIN);
 
         List<String> ingredients = getArrayObject(sandwich.getJSONArray(KEY_INGREDIENTS));
@@ -51,21 +44,16 @@ public class JsonUtils {
         return new Sandwich(mainName, alsoKnownAs, origin, description, image, ingredients);
     }
 
-    /*
-    * This method takes in a JSON array
-    * Returns a list
-    * */
-    private static List<String> getArrayObject(JSONArray array) {
+    private static List<String> getArrayObject (JSONArray jsonArray) {
         List<String> list = new ArrayList<>();
         try {
-            int x = array.length();
-            for (int y = 0; y < x; y++){
-                list.add(array.getString(y));
+            int arrayLength = jsonArray.length();
+            for (int i = 0; i < arrayLength; i++) {
+                list.add(jsonArray.getString(i));
             }
         } catch (JSONException e) {
-            Log.e(TAG, "Error", e);
+            e.printStackTrace();
         }
-
         return list;
     }
 }
